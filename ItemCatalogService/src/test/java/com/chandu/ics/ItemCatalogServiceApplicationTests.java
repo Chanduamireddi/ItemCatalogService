ab59dd82d17136f49f54ed534a8ca162afa73359
SaiChandu Amireddi <chanduamireddi@gmail.com>
2025-03-14 21:07:58 -0400

    ItemCatalogService

---
+++ ItemCatalogService/src/test/java/com/chandu/ics/ItemCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.ics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
